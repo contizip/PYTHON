6ccf35c6e76ef4b58087c56ec3abd1294fa603bb
Guilherme Conti Teixeria <68434142+contizip@users.noreply.github.com>
2024-09-10 15:01:49 -0300

    Add files via upload

--- Recebendo_dados_usuário.py
+++ Recebendo_dados_usuário.py
@@ -3,22 +3,38 @@ Recebendo dados do usuário
 """
 
 # Entrada de dados
-print("Qual o seu nome ? ")
-nome = input()
+# print("Qual o seu nome ? ")
+# nome = input()
 
-print("Seja bem-vindo(a) %s" % nome)
+# print("Seja bem-vindo(a) %s" % nome)
 
-print("Qual sua idade ? ")
-idade = input()
+# print("Qual sua idade ? ")
+# idade = input()
 
 # Processamento
 
 # Saída
-print('%s tem %s anos' % (nome, idade))
+# print('%s tem %s anos' % (nome, idade))
 
 # Outro exemplo, mas só que mais moderno para essa versão atual (versões 3.x)
 
-print('Seja bem-vindo(a) {0}' .format(nome))
+# print('Seja bem-vindo(a) {0}' .format(nome))
 
-print("Qual sua idade ? ")
-idade = input()
+# rint("Qual sua idade ? ")
+# idade = input()
+
+"""Guilherme Conti Teixeria - Faculdade UNIARARAS"""
+import datetime
+
+nome = input('Qual seu nome ?')
+
+print(f'Seja bem-vindo(a) {nome}')
+
+idade = input('Qual a sua idade ?')
+
+print(f'{nome} tem {idade} anos')
+
+ano_atual = datetime.datetime.now().year
+ano_nascimento = ano_atual - int(idade)
+
+print(f'{nome} nasceu em {ano_nascimento}')
