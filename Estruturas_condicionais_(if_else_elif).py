12da3810ce8bda98647f92625465cc6809223d19
Guilherme Conti Teixeria <68434142+contizip@users.noreply.github.com>
2024-09-13 13:31:41 -0300

    Add files via upload

---
+++ Estruturas_condicionais_(if_else_elif).py
@@ -0,0 +1,36 @@
+idade = 16
+"""
+Em C
+if(idade < 18){
+    printf('Menor de idade');
+}else if(idadede == 18){
+    printf('Maior de idade');
+}else{
+    printf(Menor de idade chefia, sem entradas aqui);
+}
+"""
+"""
+Em JAVA
+
+if(idade < 18){
+    System.out.orintln('Menor de idade');
+}else{
+    System.out.println('Maior de idade');
+}
+"""
+if idade < 18:
+    print('Não pode entrar aqui, menor de idade !')
+elif idade == 18:
+    print('Tem 18 anos, pode passar')
+elif idade == 26:
+    print('Tem 26 anos')
+else:
+    print('Maior de idade, pode entrar. Se diverta !')
+if idade < 18:
+    print('Menor de idade')
+if idade == 18:
+    print('Tem 18 anos')
+
+# A origem do elif vem da junção de else e if (elif)
+# Essas são nossas estruturas condicionais que temos no Python
+
---
+++ Tipo_float.py
@@ -0,0 +1,10 @@
+# Maneira errada
+valor = 1, 44
+
+print(valor)
+
+# Maenira certa
+
+valor = 1.44
+print(valor)
+print(type(valor))
